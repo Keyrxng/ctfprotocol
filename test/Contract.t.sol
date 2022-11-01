e1336c9afc4b11d362ef047e686c3853c4a0b232
Keyrxng <keyrxng@proton.me>
2022-11-01 21:07:22 +0000

    init

---
+++ test/Contract.t.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import 'forge-std/Test.sol';
+import { Contract } from '../src/Contract.sol';
+
+contract ContractTest is Test {
+  function setUp() public {}
+}
